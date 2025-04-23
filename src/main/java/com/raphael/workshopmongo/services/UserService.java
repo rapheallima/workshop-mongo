bcb80c8cf3d63c23c9abc96c8c35eb9b751e3ae3
Raphael Lima <raphaellima365@gmail.com>
2025-04-23 00:33:51 -0300

    Insercao de usuario com POST

--- src/main/java/com/raphael/workshopmongo/services/UserService.java
+++ src/main/java/com/raphael/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raphael.workshopmongo.domain.User;
+import com.raphael.workshopmongo.dto.UserDTO;
 import com.raphael.workshopmongo.repository.UserRepository;
 import com.raphael.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
